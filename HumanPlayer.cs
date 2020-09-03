9285b6eb4a825e57aac12eb5d56416a4a0efc5fc
Rob Mixon <mixonroberth@gmail.com>
2020-09-03 15:43:57 -0500

    updating code for try catch

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -7,7 +7,14 @@ namespace ShootingDice {
     public class HumanPlayer : Player {
         public override int Roll () {
             Console.WriteLine ("Please enter your desired dice roll");
-            int result = Int32.Parse (Console.ReadLine ());
+            int result = 0;
+            try {
+                result = Int32.Parse (Console.ReadLine ());
+            }
+            catch {
+                Console.WriteLine ("That is not a number! You now get a random number!");
+                return new Random ().Next (DiceSize) + 1;
+            }
             int whatTheyGet = new Random ().Next (DiceSize) + 1;
             Console.WriteLine ($"Was it a {result}? To bad! you get a {whatTheyGet}");
             return whatTheyGet;
